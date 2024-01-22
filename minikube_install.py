745e864c025c9e5a3c51642b50cd17c0a23f2d2b
ColinsGitCode <xueguodong2016@gmail.com>
2024-01-22 13:51:19 +0900

    Add comments for InstallMinikube.process()

--- minikube_install.py
+++ minikube_install.py
@@ -5,6 +5,18 @@ import time
 class InstallMinikube:
     @staticmethod
     def process() -> None:
+        """
+        The process of Install Minikube
+        1. Download Minikube deb package: curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+        2. Install Minikube deb package by using dkpg: dpkg -i minikube_latest_amd64.deb
+        3. Minikube start with 3 nodes and k8s v1.23.0
+            minikube start --nodes=3 --force --kubernetes-version=v1.23.0 --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook
+        4. Minikube start check: minikube kubectl -- get pods -A
+        5. Minikube Dashboard Start: minikube dashboard &
+        6. Kubectl Proxy: kubectl proxy --port=31000 --address='192.168.100.81' --accept-hosts='^.*' &
+
+        :return: None
+        """
         cmd_list = [
             "curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb",
             "dpkg -i minikube_latest_amd64.deb",
